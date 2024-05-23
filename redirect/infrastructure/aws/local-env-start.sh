3e77092890f634e84e7936f9c3fa4cbc552565fb
FlyingCow <max.cechina@gmail.com>
2024-05-23 22:51:09 +0300

    - removed ev for docker

--- redirect/infrastructure/aws/local-env-start.sh
+++ redirect/infrastructure/aws/local-env-start.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 echo "BUILDING LOCAL INFRASTRUCTURE..."
 echo " -> RUNNING DOCKER..."
 
-docker compose --env-file ./.env -f ./docker-services.local.yml up -d 
+docker compose -f ./docker-services.local.yml up -d 
 
 echo " -> DONE RUNNING DOCKER."
 
