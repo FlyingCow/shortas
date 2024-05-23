8938d7becbacc1254cdcb8f38408d919615589f4
FlyingCow <max.cechina@gmail.com>
2024-05-23 22:45:09 +0300

    - infrastructure refactoring

---
+++ redirect/infrastructure/aws/local-env-start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+echo "BUILDING LOCAL INFRASTRUCTURE..."
+echo " -> RUNNING DOCKER..."
+
+docker compose --env-file ./.env -f ./docker-services.local.yml up -d 
+
+echo " -> DONE RUNNING DOCKER."
+
+echo " -> INITIATING LOCAL ENVIRONMENT..."
+
+tflocal -chdir=./terraform apply -auto-approve -var-file="local.tfvars"
+
+echo " -> DONE INITIATING LOCAL ENVIRONMENT."
+
+
+echo " -> SEEDING SOME DATA..."
+
+chmod u+x ./test-data-seed/test-data-seed.local.sh
+./test-data-seed/test-data-seed.local.sh
+
+echo " -> DONE SEEDING SOME DATA."
+
+echo "DONE BUILDING LOCAL INFRASTRUCTURE."
---
+++ redirect/infrastructure/aws/test-data-seed/test-data-seed.local.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+echo "DATA-SEED TRACKER..."
+export AWS_DEFAULT_REGION="us-east-1"
+export LOCALSTACK_ENDPOINT_URL="http://localhost:4566"
+export AWS_ACCESS_KEY_ID="local"
+export AWS_SECRET_ACCESS_KEY="local"
+awslocal dynamodb list-tables
+
+awslocal s3 mb s3://my-bucket
+awslocal sqs create-queue --queue-name my-queue
+
+echo "------INITIALIZING DYNAMO TABLE"
+
+awslocal dynamodb put-item \
+    --table-name core-user-settings-local  \
+    --item \
+        '{"user_id": {"S": "my_users_id"}, "user_email": {"S": "my_user@email.com"}, "api_key": {"S": "my_user_api_key"}, "status": {"S": "active"}}'
+
+awslocal dynamodb put-item \
+    --table-name core-routes-local  \
+    --item \
+        '{"switch": {"S": "main"}, "link": {"S": "localhost%2ftest"}, "dest": {"S": "https://google.com"}, "owner.id": {"S": "my_users_id"}}'
+
+awslocal dynamodb put-item \
+    --table-name core-routes-local  \
+    --item \
+        '{"switch": {"S": "main"}, "link": {"S": "localhost%2fattr"}, "dest": {"S": "https://google.com"}, "owner.id": {"S": "my_users_id"}, "attributes": {"M": {"type": {"S":"test"}, "env": {"S": "local"}}}}'
+
+
+awslocal dynamodb put-item \
+    --table-name core-routes-local  \
+    --item \
+        '{"switch": {"S": "main"}, "link": {"S": "localhost%2fblocked"}, "dest": {"S": "https://google.com"}, "owner.id": {"S": "my_users_id"}, "blocked": {"BOOL": true}, "blocked.reason": {"S": "Spam"}}'
+
+
+awslocal dynamodb put-item \
+    --table-name core-routes-local  \
+    --item \
+        '{
+        "link": {
+            "S": "localhost%2fconds"
+        },
+        "routing": {
+            "M": {
+                "conditions": {
+                    "L": [
+                        {
+                            "M": {
+                                "key": {
+                                    "S": "test"
+                                },
+                                "condition": {
+                                    "M": {
+                                        "ua": {
+                                            "M": {
+                                                "IN": {
+                                                    "L": [
+                                                        {
+                                                            "S": "Edge"
+                                                        },
+                                                        {
+                                                            "S": "Chrome"
+                                                        },
+                                                        {
+                                                            "S": "Firefox"
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        },
+                                        "day_of_month": {
+                                            "M": {
+                                                "IN": {
+                                                    "L": [
+                                                        {
+                                                            "N": "7"
+                                                        },
+                                                        {
+                                                            "N": "14"
+                                                        },
+                                                        {
+                                                            "N": "30"
+                                                        },
+                                                        {
+                                                            "N": "26"
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        },
+                                        "and": {
+                                            "L": [
+                                                {
+                                                    "M": {
+                                                        "os": {
+                                                            "M": {
+                                                                "EQ": {
+                                                                    "S": "Windows"
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                "policy": {
+                    "S": "conditional"
+                }
+            }
+        },
+        "owner.id": {
+            "S": "my_users_id"
+        },
+        "switch": {
+            "S": "main"
+        }
+    }'
+
+
+awslocal dynamodb get-item --table-name core-routes-local --key '{"link": {"S": "localhost%2fconds"}, "switch": {"S": "main"}}'
+
+python3 ./add-certificate.local.py localhost ./certs/cert.pem ./certs/key.pem
